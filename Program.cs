37e3cdb52e0794fd01fa505cb68de33f6edd02c8
djiwandou-p <djiwandou@gmail.com>
2026-06-10 15:00:22 +0700

    Initial commit: SendGrid email example

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    internal class Example
+    {
+        private static void Main()
+        {
+            Execute().Wait();
+        }
+
+        static async Task Execute()
+        {
+            var apiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY");
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("noreply.pis@pertamina.com", "NoReply Test Sendgrid");
+            var subject = "Sending with SendGrid API";
+            var to = new EmailAddress("hindrawan.aris@pertamina.com", "Pak Aris");
+            var plainTextContent = "and easy to do anywhere, even with C#";
+            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            Console.WriteLine(response.StatusCode);
+        }
+    }
+}
